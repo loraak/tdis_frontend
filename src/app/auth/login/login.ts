a0a69d9feb39c959e5c7ed2729d5eac3a38d7e4e
edileili <edileila65@gmail.com>
2026-06-27 13:15:29 -0600

    Diseño para Login

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+type LoginView = 'select-role' | 'admin' | 'student';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule, CardModule, InputTextModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  currentView = signal<LoginView>('select-role');
+  
+  matricula: string = '';
 
+  setView(view: LoginView) {
+    this.currentView.set(view);
+  }
+
+  onStudentSubmit() {
+    console.log('Consultando progreso para matrícula:', this.matricula);
+  }
+
+  onAdminSubmit() {
+    console.log('Login de administrador');
+  }
 }
