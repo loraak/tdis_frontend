bc29a0b5490e3d2d97e8d8300632add17f39aa07
edileili <edileila65@gmail.com>
2026-06-28 19:43:15 -0600

    Diseño para Nueva solicitud

--- src/app/alumno/nueva-solicitud/nueva-solicitud.ts
+++ src/app/alumno/nueva-solicitud/nueva-solicitud.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-nueva-solicitud',
-  imports: [],
+  imports: [CommonModule, InputTextModule, CardModule],
   templateUrl: './nueva-solicitud.html',
   styleUrl: './nueva-solicitud.css',
 })
 export class NuevaSolicitud {
+  tipoFormulario: string = 'EVIDENCIA'; 
 
+  cambiarTipo(tipo: string): void {
+    this.tipoFormulario = tipo;
+  }
+
+  onFileSelected(event: Event): void {
+    const element = event.currentTarget as HTMLInputElement;
+    let fileList: FileList | null = element.files;
+    
+    if (fileList && fileList.length > 0) {
+      const file = fileList[0];
+      console.log('Archivo seleccionado:', file.name, 'Tamaño:', file.size);
+    }
+  }
 }
