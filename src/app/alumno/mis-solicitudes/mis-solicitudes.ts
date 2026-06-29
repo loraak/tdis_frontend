757bcdd8e28ea9cbc552c20502fc52eb1bd1afee
edileili <edileila65@gmail.com>
2026-06-28 20:16:46 -0600

    Diseño para Mis solicitudes

--- src/app/alumno/mis-solicitudes/mis-solicitudes.ts
+++ src/app/alumno/mis-solicitudes/mis-solicitudes.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-mis-solicitudes',
-  imports: [],
+  imports: [CardModule, TagModule],
   templateUrl: './mis-solicitudes.html',
   styleUrl: './mis-solicitudes.css',
 })
 export class MisSolicitudes {
+  estaComprimida: boolean = false;
 
+  toggleCompresion(): void {
+    this.estaComprimida = !this.estaComprimida;
+  }
 }
