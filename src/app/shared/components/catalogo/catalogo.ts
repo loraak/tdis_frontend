9aa974286404a007fd31b7ba1c7fff87fc360343
edileili <edileila65@gmail.com>
2026-06-28 21:00:32 -0600

    Diseño Catálogo

--- src/app/shared/components/catalogo/catalogo.ts
+++ src/app/shared/components/catalogo/catalogo.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-catalogo',
-  imports: [],
+  imports: [CardModule],
   templateUrl: './catalogo.html',
   styleUrl: './catalogo.css',
 })
