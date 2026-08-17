9da7bcf0bcb61a0a2e92c26098c7d0be5a08e80e
Karol Arvizu <lorakarvizu@gmail.com>
2026-08-17 15:48:29 -0600

    environment de producción, lol

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8089/api',
+    production: true,
+    apiUrl: 'https://api-gateway-production-2b72.up.railway.app/api',
 };
